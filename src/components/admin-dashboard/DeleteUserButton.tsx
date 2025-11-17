c483319f6d96086decc2eac0e5907d9e03c04f1f
Steve-atg <95837635+Steve-atg@users.noreply.github.com>
2025-11-17 08:55:02 +0800

    feat: implement delete user functionality with confirmation messages and refresh hook

--- src/components/admin-dashboard/DeleteUserButton.tsx
+++ src/components/admin-dashboard/DeleteUserButton.tsx
@@ -1,13 +1,27 @@
 'use client';
-import { Button } from 'antd';
+import { usersAPI } from '@/api/client';
+import { useRefresh } from '@/hooks/useRefresh';
+import { Button, message } from 'antd';
 
 interface DeleteUserButtonProps {
   id: string;
 }
 
 const DeleteUserButton = ({ id }: DeleteUserButtonProps) => {
-  const handleDeleteUser = () => {
-    console.log(`Delete user with ID: ${id}`);
+  const { refresh } = useRefresh();
+  const handleDeleteUser = async () => {
+    try {
+      const resp = await usersAPI.usersControllerRemove(id);
+      if (resp.status === 200) {
+        message.success('User deleted successfully.');
+        return;
+      }
+      message.error('Failed to delete user.');
+    } catch (error) {
+      message.error('An error occurred while deleting the user.');
+    } finally {
+      refresh();
+    }
   };
 
   return (
